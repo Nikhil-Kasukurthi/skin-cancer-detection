15e3d04cc185f3d9506326320856510f15c287fb
Nikhil Kasukurthi <nikhil.kasukurthi@gmal.com>
2017-09-07 11:20:48 +0530

    Testing data added

--- transfer_keras.py
+++ transfer_keras.py
@@ -6,22 +6,24 @@ from keras import applications
 import pickle
 # dimensions of our images.
 img_width, img_height, channels = 150, 150, 3
+train_y, validation_y, test_y = pickle.load(open('labels.pkl', 'rb'))
 
 top_model_weights_path = 'bottleneck_fc_model.h5'
 train_data_dir = './data/train'
 validation_data_dir = './data/validation'
-nb_train_samples = 2000
-nb_validation_samples = 800
+test_data_dir = './data/test'
+
+nb_train_samples = len(train_y)
+nb_validation_samples = len(validation_y)
+nb_test_samples = len(test_y)
 epochs = 50
 batch_size = 16
 
-train_y, test_y = pickle.load(open('labels.pkl','rb'))
 
 def save_bottlebeck_features():
     datagen = ImageDataGenerator(rescale=1. / 255,
                                  rotation_range=40)
 
-    # build the VGG16 network
     model = applications.mobilenet.MobileNet(input_shape=(img_width, img_height, channels), alpha=1.0, depth_multiplier=1, dropout=1e-3,
                                              include_top=False, weights='imagenet')
 
@@ -33,7 +35,7 @@ def save_bottlebeck_features():
         shuffle=False)
     bottleneck_features_train = model.predict_generator(
         generator, nb_train_samples // batch_size)
-    np.save(open('bottleneck_features_train.npy', 'w'),
+    np.save(open('bottleneck_features_train.npy', 'wb'),
             bottleneck_features_train)
 
     generator = datagen.flow_from_directory(
@@ -44,7 +46,18 @@ def save_bottlebeck_features():
         shuffle=False)
     bottleneck_features_validation = model.predict_generator(
         generator, nb_validation_samples // batch_size)
-    np.save(open('bottleneck_features_validation.npy', 'w'),
+    np.save(open('bottleneck_features_validation.npy', 'wb'),
+            bottleneck_features_validation)
+
+    generator = datagen.flow_from_directory(
+        test_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode=None,
+        shuffle=False)
+    bottleneck_features_test = model.predict_generator(
+        generator, test_y // batch_size)
+    np.save(open('bottleneck_features_test.npy', 'wb'),
             bottleneck_features_validation)
 
 
@@ -68,5 +81,10 @@ def train_top_model():
     model.save_weights(top_model_weights_path)
 
 
+def test_model():
+    test_data = np.load(open('bottleneck_features_test.npy'))
+    model.evaluate_generator(test_data)
+
 save_bottlebeck_features()
 train_top_model()
+test_model()
